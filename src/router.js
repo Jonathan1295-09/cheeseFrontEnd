f55ee3776b8dfffcf2956159754f4e1efa8ebd96
Jonathan1295-09 <jcasasola955@gmail.com>
2023-08-12 13:13:02 -0500

    changes

--- src/router.js
+++ src/router.js
@@ -7,10 +7,12 @@ import { createAction, updateAction, deleteAction} from "./actions";
 
 const router = createBrowserRouter(createRoutesFromElemnts(
     <Route path="/" element={<App/>}>
-        <Route path="" element={<Index/>} loader={cheeseloaderloader}/>
-          <Route path=":id" element={<Show/>} loader={cheesesLoaderLoader}/>
+        <Route path="" element={<Index/>} loader={cheeseloader}/>
+          <Route path=":id" element={<Show/>} loader={cheesesLoader}/>
           <Route path="/create" action={createAction}/>
           <Route path="update/:id" action={updateAction}/>
           <Route path="delete/:id"  action={deleteAction}/>
     </Route>
 ))
+
+export default router;
